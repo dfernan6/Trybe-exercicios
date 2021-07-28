fd38b9dedd6225342350d7b24a50f0b7f55722ed
dfernan6 <danilo.afernandes@outlook.com>
2021-07-28 15:55:11 -0300

    Atividades JS #1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/scripts2.js
@@ -0,0 +1,12 @@
+let nota = 82;
+
+if (nota >= 80){
+    console.log("Você foi aprovada"
+    );
+}
+else if (nota < 80 && nota >= 60) {
+    console.log("Voce esta na lista de espera");
+}
+else {
+    console.log("Voce foi reprovado");
+}
