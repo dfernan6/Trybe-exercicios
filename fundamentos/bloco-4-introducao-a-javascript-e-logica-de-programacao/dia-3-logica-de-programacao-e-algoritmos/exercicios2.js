63fb355fb7fd423513fbd19ff1a005ed58cdd5c2
dfernan6 <danilo.afernandes@outlook.com>
2021-07-31 04:16:20 -0300

    eee mais alterações

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicios2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicios2.js
@@ -1,10 +1,8 @@
 let asterisco = "*";
-//let sinal = "*";
+let sinal = "*";
 
 for( let contador = 1 ; contador <= 5 ; contador += 1){
        asterisco = asterisco + contador;
-       for (let linha = 1 ; linha <= 5 ; contador += 1){
-       asterisco = asterisco + linha;
-         console.log(asterisco);
-}
+       asterisco = asterisco + sinal;
+       console.log(asterisco);
 }
