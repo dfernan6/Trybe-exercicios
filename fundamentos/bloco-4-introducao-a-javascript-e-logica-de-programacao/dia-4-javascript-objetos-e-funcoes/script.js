a0b1f0f129daa0ff4dbd7b36f65b487ac8806acc
dfernan6 <danilo.afernandes@outlook.com>
2021-08-02 15:47:31 -0300

    Conclusao do primeiro objeto

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -6,9 +6,16 @@ let medals = { golden: 2, silver: 3 };
 let player = {
     name: "Marta",
     lastName: "Silva",
-    age: 34
+    age: 34,
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
 }
-  console.table(player);
   console.log("A jogadora " + player["name"] + " " +
    player["lastName"] + " " + "tem " + player["age"] +
-   " de idade") // objeto feito acima player
+   " anos de idade e foi considerada a melhor do mundo em " + 
+   player["bestInTheWorld"] + " ou seja, a jogadora " +
+    player["name"] + " " + player["lastName"] + " " +
+     "tem " + player.bestInTheWorld.length + " titulos mundiais."); 
+  console.log("A jogadora possui " + medals["golden"] +
+
+  " medalhas de ouro e " + medals["silver"] + " medalhas de prata." );
+  // objeto feito acima player
