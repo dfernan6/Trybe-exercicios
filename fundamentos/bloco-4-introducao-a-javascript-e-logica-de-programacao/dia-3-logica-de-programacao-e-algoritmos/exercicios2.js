85865f6bf319a7790a7a6f073c28031b0289ed8f
dfernan6 <danilo.afernandes@outlook.com>
2021-08-02 14:10:19 -0300

    Atualizando alterações de pastas not merged

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicios2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicios2.js
@@ -3,6 +3,7 @@ let sinal = "*";
 
 for( let contador = 1 ; contador <= 5 ; contador += 1){
        asterisco = asterisco + contador;
-       sinal = sinal + asterisco;
-       console.log(sinal);
+       asterisco = sinal + asterisco;
+       asterisco = "*";
+       console.log(asterisco);
 }
