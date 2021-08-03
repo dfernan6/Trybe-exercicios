ac332a906f01d64c50e9b727a6cb0718ab7ade33
dfernan6 <danilo.afernandes@outlook.com>
2021-08-03 00:19:21 -0300

    mais uma atualização #4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio4.js
@@ -0,0 +1,7 @@
+let meuArray = [2, 3, 6, 7, 10, 1];
+let maior = Math.max.apply(null, meuArray );
+
+let otroArray = [2, 4, 6, 7, 10, 0, -3]; 
+
+let menor = Math.min.apply(null, otroArray );
+console.log(menor);
