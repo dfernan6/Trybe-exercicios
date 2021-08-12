9603e86bcba68769ca1e6aa6bfd0bc551fb42423
dfernan6 <danilo.afernandes@outlook.com>
2021-08-12 14:40:32 -0300

    Enviando os exercicios da aula 5.1 3 functions

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/trybe.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/trybe.js
@@ -1,12 +1,7 @@
 
-let pilotosDeF1 = document.getElementsByClassName("cpiloto-f1-atual")
-
-for (let index = 0; index < pilotosDeF1.length; index += 1){
-    pilotosDeF1[index].innerText = "Lewis Hammilton";
-}
-
 let primeiroPiloto = document.getElementsByClassName("cpiloto-f1-atual")[0];
 primeiroPiloto.style.color= "white";
 
 let pilotoDivino = document.getElementsByTagName("p")[0];
 pilotoDivino.style.color= "white";
+pilotoDivino.innerHTML = "<b>Ayrton Senna</b>";
