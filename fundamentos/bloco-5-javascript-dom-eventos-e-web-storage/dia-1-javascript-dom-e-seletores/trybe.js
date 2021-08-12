9b1444047e55abe4a7c8dc04619a33c1b8bbb3ce
dfernan6 <danilo.afernandes@outlook.com>
2021-08-12 11:58:44 -0300

    Mais alteraçoes no trybe.js trocando nomde de todos na classe especifica

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/trybe.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/trybe.js
@@ -3,3 +3,9 @@ let paragraph = document.getElementById("paragraph");
 paragraph.style.color="blue";
 let paragraph2 = document.getElementById("second-paragraph");
 paragraph2.style.color="green";
+
+let pilotosDeF1 = document.getElementsByClassName("cpiloto-f1-atual")
+
+for (let index = 0; index < pilotosDeF1.length; index += 1){
+    pilotosDeF1[index].innerText = "Lewis Hammilton";
+}
