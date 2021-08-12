2169d5237bfc9072efc898c3dac9246682ed0b0e
dfernan6 <danilo.afernandes@outlook.com>
2021-08-11 23:42:48 -0300

    Segunda atualizaçao dos arquivos dai 5.1

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/trybe.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/trybe.js
@@ -1,2 +1,5 @@
-console.log("Olá Mundo!")
-console.log("Como andam as coisas?")
+console.log(document.getElementById("iTextoTopo").innerHTML="Aprendizado da <b>aula</b> de hoje");
+let paragraph = document.getElementById("paragraph");
+paragraph.style.color="blue";
+let paragraph2 = document.getElementById("second-paragraph");
+paragraph2.style.color="green";
