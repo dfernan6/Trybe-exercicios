b28675cf3d2f3ecab919cd18d81eb40e7f2aeda9
dfernan6 <danilo.afernandes@outlook.com>
2021-08-12 12:49:30 -0300

    alterando elementos pelo tagname

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/trybe.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/trybe.js
@@ -1,11 +1,12 @@
-console.log(document.getElementById("iTextoTopo").innerHTML="Aprendizado da <b>aula</b> de hoje");
-let paragraph = document.getElementById("paragraph");
-paragraph.style.color="blue";
-let paragraph2 = document.getElementById("second-paragraph");
-paragraph2.style.color="green";
 
 let pilotosDeF1 = document.getElementsByClassName("cpiloto-f1-atual")
 
 for (let index = 0; index < pilotosDeF1.length; index += 1){
     pilotosDeF1[index].innerText = "Lewis Hammilton";
 }
+
+let primeiroPiloto = document.getElementsByClassName("cpiloto-f1-atual")[0];
+primeiroPiloto.style.color= "white";
+
+let pilotoDivino = document.getElementsByTagName("p")[0];
+pilotoDivino.style.color= "white";
