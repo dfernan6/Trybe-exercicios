4f8c2618b5fb416f683acde50c0a9b8adce5e5ae
dfernan6 <danilo.afernandes@outlook.com>
2021-08-14 17:49:05 -0300

    Atividades 2 do dia 5.2

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios.js
@@ -0,0 +1,20 @@
+let ingredientItems = [
+    "500g de feijão carioquinha cozido",
+    "200g de toucinho",
+    "1 concha de óleo",
+    "1 cebola media picada",
+    "4 dentes de alho",
+    "5 ovos",
+    "1 colher de sopa de sal com alho"
+];
+
+let ingredientList = document.querySelector(".ingredients-list");
+
+for (let index = 0; index < ingredientItems.length; index += 1){
+    let ingredient = ingredientItems[index];
+
+    let ingredientListItem = document.createElement("li");
+    ingredientListItem.innerHTML = ingredient;
+
+    ingredientList.appendChild(ingredientList);
+}
