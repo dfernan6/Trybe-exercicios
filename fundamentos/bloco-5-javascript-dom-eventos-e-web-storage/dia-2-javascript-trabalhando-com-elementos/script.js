0fb5de2bd2e88b21d0d9ed91a954e4251ad185c3
dfernan6 <danilo.afernandes@outlook.com>
2021-08-15 23:28:25 -0300

    Aula pt 2 dia 5.2 finalizada

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,6 +1,9 @@
+let filhinho = "Beware!";
 let textinho = "Hello Friends";
-
 let textoInserido = document.querySelector("#elementoOndeVoceEsta");
 let creatingBrother = document.createElement("p");
 creatingBrother.innerText = textinho;
-textoInserido.appendChild(creatingBrother)
+textoInserido.appendChild(creatingBrother);
+let marcandoFilhos = document.querySelector("#primeiroFilhoDoFilho");
+let creatingChild = document.createElement("p");
+creatingChild.innerText = filhinho;
