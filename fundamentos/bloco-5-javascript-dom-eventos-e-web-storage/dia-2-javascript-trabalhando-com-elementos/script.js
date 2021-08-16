4c454b1a7fb0c7dcfb1b3c1249cc12b99b195562
dfernan6 <danilo.afernandes@outlook.com>
2021-08-16 02:23:02 -0300

    enviado a finaliza aulas pt 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -7,3 +7,19 @@ textoInserido.appendChild(creatingBrother);
 let marcandoFilhos = document.querySelector("#primeiroFilhoDoFilho");
 let creatingChild = document.createElement("p");
 creatingChild.innerText = filhinho;
+
+function apagaFilhos(pai) {
+    const children = pai.children
+    for (let index = 0; index < children.length; index += 1) {
+      child = children[index]
+      childName = child.id
+      if (childName === 'pai' || childName === 'elementoOndeVoceEsta' || childName === 'primeiroFilhoDoFilho') {
+        apagaFilhos(child)
+      } else {
+        child.remove()
+        index -= 1
+      }
+    }
+  }
+  console.log(apagaFilhos());
+
