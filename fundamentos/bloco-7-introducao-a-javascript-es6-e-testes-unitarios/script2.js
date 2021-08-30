6fe7dff63ed1238f9829bcee42d3af60942a90c8
dfernan6 <danilo.afernandes@outlook.com>
2021-08-30 16:14:59 -0300

    atividades Object.values concluidas

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/script2.js
@@ -0,0 +1,44 @@
+//Parte III - Object.values
+
+const coolestTvShow = {
+    name: "BoJack Horseman",
+    genre: "adult animation",
+    createdBy: "Raphael Bob-Waksberg",
+    favoriteCharacter: "Princess Carolyn",
+    quote: "Princess Carolyn always lands on her feet.",
+    seasons: 6,
+  };
+  
+  for (const property in coolestTvShow) {
+    console.log(coolestTvShow[property]);
+  }
+
+  //Podemos então refatorar para utilizar o Object.values ,
+  // nosso código ficaria assim:
+  console.log(Object.values(coolestTvShow));  
+
+  const student = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkill: 'Ótimo',
+  };
+  
+  const listSkillsValuesWithFor = (student) => {
+    const skills = [];
+    for(skill in student) {
+      skills.push(student[skill]);
+    }
+  
+    return skills;
+  };
+  
+  const listSkillsValuesWithValues = (student) => Object.values(student);
+  
+  // Sem Object.values
+  console.log(listSkillsValuesWithFor(student));
+  
+  // Com Object.values
+  console.log(listSkillsValuesWithValues(student));
+  // Observe como a função listSkillsValuesWithValues 
+  //é bem mais simples e resolve o problema da listagem de valores de forma mais direta.
