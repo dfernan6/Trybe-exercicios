d283771d11adb83fa91b10ed201b76c2fb1b7aa4
dfernan6 <danilo.afernandes@outlook.com>
2021-09-02 00:55:34 -0300

    Enviando segunda atividade realizada na aula

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/script.js
@@ -10,3 +10,4 @@ assert.ok(expected === 3, 'Um mais dois é igual a três'); // Checa se o primei
 assert.strictEqual(expected, 3, 'Um mais dois é igual a três'); // Checa se o primeiro e segundo argumentos são iguais em valor e tipo (===)
 assert.equal(expected, '3', 'Um mais dois é igual a três'); // não faz check se esse valor é string
 assert.notStrictEqual(expected, 4, 'Um mais dois é igual a três (e não quatro!)'); // Checa se o primeiro e segundo argumentos são diferentes (!==)
+
