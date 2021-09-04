ab4c5ba12dfb2ac761520340d3f06a824e65f48f
dfernan6 <danilo.afernandes@outlook.com>
2021-09-04 01:36:19 -0300

    Enviando as respostas ex2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios.js
@@ -9,3 +9,12 @@ const testingScope = (escopo) => {
     console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
 }
   testingScope(true);
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+  function compararNumeros(a, b) {
+    return a - b;
+  }
+  oddsAndEvens.sort(compararNumeros)// Seu código aqui.
+
+  console.log(`Ordenando esses números ${oddsAndEvens} e também
+  adicionando template string`); // será necessário alterar essa linha 😉
