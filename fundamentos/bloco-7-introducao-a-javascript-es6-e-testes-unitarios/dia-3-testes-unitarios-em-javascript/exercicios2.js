0e227484b44edd498c75083c60d3639052fbf9ee
dfernan6 <danilo.afernandes@outlook.com>
2021-09-05 19:17:41 -0300

    Enviando a resposta da 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/exercicios2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/exercicios2.js
@@ -10,6 +10,13 @@ function myRemove(arr, item) {
     return newArr;
 }
 // implemente seus testes aqui
-myRemove([1, 2, 3, 4], 3);
-assert.notStrictEqual([1, 2, 4], 3, 'Feito a remoção do item');
-assert.strictEqual([1, 2, 3, 4], 3, 3, 'Feito a remoção do item');
+const expected = myRemove([1, 2, 3, 4], 3);
+assert.strictEqual(typeof myRemove, 'function', 'Feito a remoção do item');//colocado primeiramente o typo da função
+assert.deepStrictEqual(expected, [ 1, 2, 4 ], 'Feito a remoção do item');
+assert.notDeepStrictEqual(expected, [1, 2, 3, 4], 'há diferenças');
+
+const myList = [5, 6, 7, 8];
+myRemove(myList, 5)
+assert.deepStrictEqual( [5, 6, 7, 8], [5, 6, 7, 8]);
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
