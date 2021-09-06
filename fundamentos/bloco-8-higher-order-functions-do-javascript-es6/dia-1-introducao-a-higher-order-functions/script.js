cbdea82972f2d364c7a3b08d9ec8acf8d17c45bb
dfernan6 <danilo.afernandes@outlook.com>
2021-09-06 15:33:40 -0300

    Criando mais um arquivo JS

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-introducao-a-higher-order-functions/script.js
@@ -1,8 +1,25 @@
-function sum (number1, number2) {
+const sum = (number1, number2) => {
     return number1 + number2;
   }
   
-  const sumVariable = sum;
+  const sumVariable = sum(1, 2);
   
   console.log(sumVariable);
   //  [Function: sum]
+
+  const sayHello = () => {
+    return ('hello trybers');
+  }
+  //Passar funções como argumento para outras funções:
+  const printGreeting = (callback) => {
+      console.log(callback());
+  }
+  //Retornar uma função de outra função:
+  printGreeting(sayHello);
+
+  const sumFixAmount = (amount) => {
+    return (number) => amount + number;
+  }
+  
+  const initialSum = sumFixAmount(15)
+  console.log(initialSum(5));
