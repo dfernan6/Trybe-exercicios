4f3dfd3d1a16744af5ef6d7410ecc67cd4cc2353
dfernan6 <danilo.afernandes@outlook.com>
2021-10-11 00:49:33 -0300

    Enviando fixaçao dia 10.2 #2

--- desenvolvimento-front-end/dia-2-componentes-react/aula-10.2/src/App.js
+++ desenvolvimento-front-end/dia-2-componentes-react/aula-10.2/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Order from './Order';
+import UserProfile from './UserProfile';
 
 class App extends React.Component {
   render() {
@@ -23,13 +24,36 @@ class App extends React.Component {
         currency: "dollars"
       }
     };
-
+    const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+    const items = shoppingList.map((item, index) => (<li key={ index }>{ item }</li>));
+    const users = [{
+      id: 102,
+      name: "João",
+      email: "joao@gmail.com",
+      avatar: "https:\/\/cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+    },
+    {
+      id: 77,
+      name: "Amélia",
+      email: "amelia@gmail.com",
+      avatar: "https:\/\/cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+    }
+  ];
     return (
+      <section>
       <div className="App">
         <h1> Orders recently created </h1>
         < Order order={ headphone }/>
         < Order order={ energyDrink } />
+        <h2>Lista de compras</h2>
+        <ul>
+          { items }
+        </ul>
       </div>
+      <div className="App">
+      {users.map(user => <UserProfile user={user} />)}
+      </div>
+      </section>
     );
   }
 }
