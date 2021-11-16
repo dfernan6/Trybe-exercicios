224736e71a3c2d39abd8ed6b7db676ec9549b876
dfernan6 <danilo.afernandes@outlook.com>
2021-11-16 01:38:20 -0300

    Enviando exercicios de fixaçao n6

--- desenvolvimento-front-end/blocl-12-colco-de-vida-de-componentes-e-react-router/dia-2-react-router/training-skills/src/App.js
+++ desenvolvimento-front-end/blocl-12-colco-de-vida-de-componentes-e-react-router/dia-2-react-router/training-skills/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
+import LoginForm from './componentes/LoginForm';
+import RegisterForm from './componentes/RegisterForm';
 // import mainPage from './componentes/mainPage'
 
 class App extends React.Component {
   render (){
     return (
     <main className="App">
-    <section className="login-form">
-      <h1>Login</h1>
-    </section>
-    <section className="register-form">
-      <h1>Register</h1>
-    </section>
+      <LoginForm />
+      <RegisterForm />
     </main>
     )
   }
---
+++ desenvolvimento-front-end/blocl-12-colco-de-vida-de-componentes-e-react-router/dia-2-react-router/training-skills/src/componentes/RegisterForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class RegisterForm extends React.Component {
+  render() {
+    return (
+      <section className="register-form">
+      <h1>Register</h1>
+      </section>
+    );
+}
+}
+
+export default RegisterForm;
